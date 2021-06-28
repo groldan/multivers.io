4e61a75098bcc72fe78657e574e60805f493b789
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-06-28 20:26:26 -0300

    Initial feature-service skeleton

---
+++ services/features/src/main/java/io/multivers/features/FeatureService.java
@@ -0,0 +1,12 @@
+package io.multivers.features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureService.class, args);
+    }
+}
---
+++ services/features/src/test/java/io/multivers/features/FeatureServiceTests.java
@@ -0,0 +1,11 @@
+package io.multivers.features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureServiceTests {
+
+    @Test
+    void contextLoads() {}
+}
